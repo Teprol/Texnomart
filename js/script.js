d821a7228aae876408704ff8dc58cda6485dbd39
Teprol <78510204+Teprol@users.noreply.github.com>
2022-04-12 23:38:17 +0400

    доделал анимацию попапа и анимацию с проверкой

--- js/script.js
+++ js/script.js
@@ -3,44 +3,47 @@ const popupFeedback = document.querySelector('.feedback');
 const closeFeedback  = popupFeedback.querySelector('.feedback__close');
 const loginFeedback = popupFeedback.querySelector('[name=имя]');
 const mailFeedback = popupFeedback.querySelector('[name=почта]');
-const formFeedback = popupFeedback.querySelector('form');
+const submitFeedback = popupFeedback.querySelector('.feedback__form-submit input');
+const textFeedback = popupFeedback.querySelector('textarea');
+//переменнные для localStorage и открытия закрытия попапа
 
 let storageSupport = true;
 let storage;
+//нужны для проверки на ошибки
 
 try {
   storage = localStorage.getItem('loginFeedback');
 } catch {
   storageSupport = false;
 }
+// проверка на поддержку localStorage, елси фолс то дальше запись не сделается в ключ
 
 buttonPopup.addEventListener('click', function () {
-  popupFeedback.classList.add('feedback_open');
+  popupFeedback.classList.add('feedback_open'); // откроет попап
   if (storage) {
     loginFeedback.value = storage;
-    mailFeedback.focus();
+    mailFeedback.focus();  //если запись в логине есть то кинет фокус на мыло
   } else {
-    loginFeedback.focus();
+    loginFeedback.focus(); //если логина нет, то на него фоку
   };
 });
 
 closeFeedback.addEventListener('click', function () {
   popupFeedback.classList.remove('feedback_open');
   popupFeedback.classList.remove('feedback_error');
-});
+}); // закрытие попапа и удаление всех классов с него
 
-formFeedback.addEventListener('submit', function (evt) {
-  if (!loginFeedback.value || !mailFeedback.value) {
-    evt.preventDefault();
+submitFeedback.addEventListener('click', function () {
+  if (!loginFeedback.value || !mailFeedback.value || !textFeedback.value) { // провекрка на незаполнение логина и мыла если не заполнено то присвоет анимацтю покачивания попапа
     popupFeedback.classList.add('feedback_error');
-  } else {
-    if (storageSupport) {
+  } else { // если все гуд то запишет логин
+    if (storageSupport) { //если стораж поддерживается
       localStorage.setItem('loginFeedback', loginFeedback.value);
     }
   }
 });
 
-window.addEventListener('keydown', function (evt) {
+window.addEventListener('keydown', function (evt) { // если попап открыт то можно закрыть по esc
 if (evt.keyCode === 27) {
   if (popupFeedback.classList.contains('feedback_open')) {
     evt.preventDefault();
